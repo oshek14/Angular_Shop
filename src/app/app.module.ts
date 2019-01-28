ff7a92ddba6888e537166e2aeedae906f57462e2
novaknole <glagh14@freeuni.edu.ge>
2019-01-28 20:49:57 +0400

    added ngbdropdown bootstrap library and made it directive to dropdown list in navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth'
 import { RouterModule } from '@angular/router'
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 
 
 
@@ -40,6 +40,7 @@ import { LoginComponent } from './login/login.component';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFireDatabaseModule,
+    NgbModule.forRoot(),
     RouterModule.forRoot([
       {path : '', component: HomeComponent},
       {path : 'products', component: ProductsComponent},
